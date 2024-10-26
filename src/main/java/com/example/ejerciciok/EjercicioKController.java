53bc67a9f4d39c25889d1ec7cccc82e6d99d4456
Joelgs13 <050213jgonzalez@gmail.com>
2024-10-26 16:42:52 +0200

    hecho el FXML y el esqueleto del controlador

---
+++ src/main/java/com/example/ejerciciok/EjercicioKController.java
@@ -0,0 +1,28 @@
+package com.example.ejerciciok;
+
+
+import javafx.fxml.FXML;
+import javafx.scene.image.ImageView;
+
+public class EjercicioKController {
+
+    @FXML
+    private ImageView ivHorasDerecha;
+
+    @FXML
+    private ImageView ivHorasIzquierda;
+
+    @FXML
+    private ImageView ivMinutosDerecha;
+
+    @FXML
+    private ImageView ivMinutosIzquierda;
+
+    @FXML
+    private ImageView ivSegundosDerecha;
+
+    @FXML
+    private ImageView ivSegundosIzquierda;
+
+
+}
